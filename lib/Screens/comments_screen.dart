90d0d3aa018cbf9aba73bc8ccecf2b0660ac8bd2
muzzammil763 <deadbase763@gmail.com>
2024-02-19 14:58:44 +0500

    added listview builder and complete ui of comments_screen.dart

--- lib/Screens/comments_screen.dart
+++ lib/Screens/comments_screen.dart
@@ -49,6 +49,181 @@ class _CommentsScreenState extends State<CommentsScreen> {
           ),
         ),
       ),
+      body: FutureBuilder(
+        future: getComments(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return RefreshIndicator(
+            onRefresh: () async {
+              getComments();
+            },
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: commentsModel.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(top: 8, left: 8, right: 8),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.blueAccent.withOpacity(0.2),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 24,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                text: 'Post ID: ',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueAccent[700],
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: '${commentsModel[index].postId}',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                text: 'ID: ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.blueAccent[700],
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: '${commentsModel[index].id}',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: Colors.blueAccent.withOpacity(0.3),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 16,
+                            ),
+                            child: Row(
+                              children: [
+                                const Spacer(),
+                                RichText(
+                                  text: TextSpan(
+                                    text: 'Email: ',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Colors.blueAccent[700],
+                                    ),
+                                    children: <TextSpan>[
+                                      TextSpan(
+                                        text: commentsModel[index].email,
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                ),
+                                const Spacer(),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4.0),
+                          child: RichText(
+                            text: TextSpan(
+                              text: 'Name: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.blueAccent[700],
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: commentsModel[index].name,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4.0),
+                          child: RichText(
+                            text: TextSpan(
+                              text: 'Body: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.blueAccent[700],
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: commentsModel[index].body,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
