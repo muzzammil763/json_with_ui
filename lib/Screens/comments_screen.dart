a1db52821a53f33b22d6565a13e1f65557484fd5
muzzammil763 <deadbase763@gmail.com>
2024-02-19 14:46:57 +0500

    api hit done

--- lib/Screens/comments_screen.dart
+++ lib/Screens/comments_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:json_with_ui/Models/comments_model.dart';
+import 'package:http/http.dart' as http;
 
 class CommentsScreen extends StatefulWidget {
   const CommentsScreen({super.key});
@@ -8,8 +11,44 @@ class CommentsScreen extends StatefulWidget {
 }
 
 class _CommentsScreenState extends State<CommentsScreen> {
+  List<CommentsModel> commentsModel = [];
+  Future<List<CommentsModel>> getComments() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/comments'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        commentsModel.add(CommentsModel.fromJson(i));
+      }
+      return commentsModel = commentsModel;
+    } else {
+      return commentsModel = [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.blueAccent[700],
+        title: const Text(
+          'Comments Screen',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
