eff41205999c8418aee2f10d9c101e98cd91a7f8
muzzammil763 <deadbase763@gmail.com>
2024-02-19 15:04:35 +0500

    api call done for albums screen

--- lib/Screens/albums_screen.dart
+++ lib/Screens/albums_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:json_with_ui/Models/albums_model.dart';
 
 class AlbumsScreen extends StatefulWidget {
   const AlbumsScreen({super.key});
@@ -8,8 +11,44 @@ class AlbumsScreen extends StatefulWidget {
 }
 
 class _AlbumsScreenState extends State<AlbumsScreen> {
+  List<AlbumsModel> albumsModel = [];
+  Future<List<AlbumsModel>> getAlbums() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        albumsModel.add(AlbumsModel.fromJson(i));
+      }
+      return albumsModel = albumsModel;
+    } else {
+      return albumsModel = [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.blueAccent[700],
+        title: const Text(
+          'Albums Screen',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
