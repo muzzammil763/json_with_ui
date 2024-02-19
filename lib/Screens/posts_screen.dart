8946457630eff980de5258afcad69d0a8293e2d1
muzzammil763 <deadbase763@gmail.com>
2024-02-19 14:09:08 +0500

    data parsing succeeded + Ui

--- lib/Screens/posts_screen.dart
+++ lib/Screens/posts_screen.dart
@@ -1,7 +1,30 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:json_with_ui/Models/posts_model.dart';
+import 'package:http/http.dart' as http;
 
-class PostsScreen extends StatelessWidget {
+class PostsScreen extends StatefulWidget {
   const PostsScreen({super.key});
+
+  @override
+  State<PostsScreen> createState() => _PostsScreenState();
+}
+
+class _PostsScreenState extends State<PostsScreen> {
+  List<PostsModel> postsModel = [];
+  Future<List<PostsModel>> getPosts() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        postsModel.add(PostsModel.fromJson(i));
+      }
+      return postsModel = postsModel;
+    } else {
+      return postsModel = [];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +48,150 @@ class PostsScreen extends StatelessWidget {
             color: Colors.white,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        future: getPosts(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return RefreshIndicator(
+            onRefresh: () async {
+              getPosts();
+            },
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: postsModel.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(top: 8, left: 8, right: 8),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.blueAccent.withOpacity(0.2),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 24,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                text: 'User ID: ',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueAccent[700],
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: '${postsModel[index].userId}',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            RichText(
+                              text: TextSpan(
+                                text: 'ID: ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.blueAccent[700],
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: '${postsModel[index].id}',
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: Colors.blueAccent.withOpacity(0.3),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 16,
+                            ),
+                            child: RichText(
+                              text: TextSpan(
+                                text: 'Title: ',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.blueAccent[700],
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: postsModel[index].title,
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4.0),
+                          child: RichText(
+                            text: TextSpan(
+                              text: 'Body: ',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.blueAccent[700],
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: postsModel[index].body,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
