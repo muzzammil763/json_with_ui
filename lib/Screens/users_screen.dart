535b1e6d6fe4ce6430f7b91189f6f9097ba18271
muzzammil763 <deadbase763@gmail.com>
2024-02-20 15:36:18 +0500

    added some component of ui

--- lib/Screens/users_screen.dart
+++ lib/Screens/users_screen.dart
@@ -49,6 +49,174 @@ class _UsersScreenState extends State<UsersScreen> {
           ),
         ),
       ),
+      body: FutureBuilder(
+        future: getUsers(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return RefreshIndicator(
+            onRefresh: () async {
+              getUsers();
+            },
+            child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemCount: usersModel.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(top: 8, left: 8, right: 8),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.blueAccent.withOpacity(0.2),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                              left: 16, right: 8, top: 16, bottom: 16),
+                          height: 100,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                              left: 8, right: 8, top: 16, bottom: 16),
+                          height: 100,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                              left: 8, right: 16, top: 16, bottom: 16),
+                          height: 100,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // child: Padding(
+                  //   padding: const EdgeInsets.symmetric(
+                  //     horizontal: 20,
+                  //     vertical: 24,
+                  //   ),
+                  //   child: Column(
+                  //     crossAxisAlignment: CrossAxisAlignment.start,
+                  //     children: [
+                  //       Container(
+                  //         color: Colors.amberAccent,
+                  //         child: Padding(
+                  //           padding: const EdgeInsets.all(16),
+                  //           child: Column(
+                  //             children: [
+                  //               RichText(
+                  //                 text: TextSpan(
+                  //                   text: 'ID: ',
+                  //                   style: TextStyle(
+                  //                     fontSize: 16,
+                  //                     fontWeight: FontWeight.bold,
+                  //                     color: Colors.blueAccent[700],
+                  //                   ),
+                  //                   children: <TextSpan>[
+                  //                     TextSpan(
+                  //                       text: '${usersModel[index].id}',
+                  //                       style: const TextStyle(
+                  //                         color: Colors.black,
+                  //                       ),
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //               ),
+                  //               RichText(
+                  //                 text: TextSpan(
+                  //                   text: 'Name: ',
+                  //                   style: TextStyle(
+                  //                     fontWeight: FontWeight.bold,
+                  //                     fontSize: 16,
+                  //                     color: Colors.blueAccent[700],
+                  //                   ),
+                  //                   children: <TextSpan>[
+                  //                     TextSpan(
+                  //                       text: usersModel[index].name,
+                  //                       style: const TextStyle(
+                  //                         color: Colors.black,
+                  //                       ),
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //               ),
+                  //               RichText(
+                  //                 text: TextSpan(
+                  //                   text: 'Username: ',
+                  //                   style: TextStyle(
+                  //                     fontWeight: FontWeight.bold,
+                  //                     fontSize: 16,
+                  //                     color: Colors.blueAccent[700],
+                  //                   ),
+                  //                   children: <TextSpan>[
+                  //                     TextSpan(
+                  //                       text: usersModel[index].username,
+                  //                       style: const TextStyle(
+                  //                         color: Colors.black,
+                  //                       ),
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //                 overflow: TextOverflow.ellipsis,
+                  //                 maxLines: 1,
+                  //               ),
+                  //               RichText(
+                  //                 text: TextSpan(
+                  //                   text: 'Email: ',
+                  //                   style: TextStyle(
+                  //                     fontWeight: FontWeight.bold,
+                  //                     fontSize: 16,
+                  //                     color: Colors.blueAccent[700],
+                  //                   ),
+                  //                   children: <TextSpan>[
+                  //                     TextSpan(
+                  //                       text:
+                  //                           usersModel[index].email.toString(),
+                  //                       style: const TextStyle(
+                  //                         color: Colors.black,
+                  //                         fontWeight: FontWeight.normal,
+                  //                       ),
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //                 maxLines: 2,
+                  //                 overflow: TextOverflow.ellipsis,
+                  //               ),
+                  //             ],
+                  //           ),
+                  //         ),
+                  //       )
+                  //     ],
+                  //   ),
+                  // ),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
