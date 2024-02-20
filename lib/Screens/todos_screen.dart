5b1e31a7a0595574023e6be21d1655222dad1f4c
muzzammil763 <deadbase763@gmail.com>
2024-02-20 15:07:10 +0500

    api hit successfully

--- lib/Screens/todos_screen.dart
+++ lib/Screens/todos_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:json_with_ui/Models/todos_model.dart';
 
 class TodosScreen extends StatefulWidget {
   const TodosScreen({super.key});
@@ -8,6 +11,21 @@ class TodosScreen extends StatefulWidget {
 }
 
 class _TodosScreenState extends State<TodosScreen> {
+  List<TodosModel> todosModel = [];
+  Future<List<TodosModel>> getTodos() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        todosModel.add(TodosModel.fromJson(i));
+      }
+      return todosModel = todosModel;
+    } else {
+      return todosModel = [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
