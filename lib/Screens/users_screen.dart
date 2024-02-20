4dda2a95c491925a93307a83d0c7f729e4826acf
muzzammil763 <deadbase763@gmail.com>
2024-02-20 15:19:27 +0500

    api hitted

--- lib/Screens/users_screen.dart
+++ lib/Screens/users_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:json_with_ui/Models/users_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class UsersScreen extends StatefulWidget {
   const UsersScreen({super.key});
@@ -8,6 +11,21 @@ class UsersScreen extends StatefulWidget {
 }
 
 class _UsersScreenState extends State<UsersScreen> {
+  List<UsersModel> usersModel = [];
+  Future<List<UsersModel>> getUsers() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        usersModel.add(UsersModel.fromJson(i));
+      }
+      return usersModel = usersModel;
+    } else {
+      return usersModel = [];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
